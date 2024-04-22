31e679a5c40367499abfe5b679f83aaa33bfaca7
lonelypuppy <118647548+GURESPEX@users.noreply.github.com>
2024-04-22 17:22:03 +0700

    [Edit]: Change container responsive style

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="w-screen h-screen col items-center bg-gradient-to-b from-black to-lime-950 bg-fixed overflow-auto">
       <div className="col container row w-full max-w-screen-lg">
-        <div className="row lg:col items-center gap-16 p-16">
+        <div className="col items-center gap-16 p-16">
           <CardOne />
           <CardTwo />
         </div>
